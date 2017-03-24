dc05e2d96a8a52ca257dd879ff060d7173604879
Ivan Korolevskiy <ivan.korolevskiy@gmail.com>
2017-03-23 23:58:35 -0700

    add nginix

--- apache/run.sh
+++ apache/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# PHP
 PHP_ERROR_REPORTING=${PHP_ERROR_REPORTING:-"E_ALL & ~E_DEPRECATED & ~E_NOTICE"}
 sed -ri 's/\;date\.timezone\ \=/date\.timezone\ \=\ America\/Los_Angeles/g' /etc/php5/cli/php.ini
 sed -ri 's/\;date\.timezone\ \=/date\.timezone\ \=\ America\/Los_Angeles/g' /etc/php5/apache2/php.ini
@@ -14,10 +15,11 @@ sed -ri 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g' /etc/php5/apach
 echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php5/apache2/php.ini
 echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php5/cli/php.ini
 
-#sendmail
+# sendmail
 line=$(head -n 1 /etc/hosts)
 line2=$(echo $line | awk '{print $2}')
 echo "$line $line2.localdomain" >> /etc/hosts
 /etc/init.d/sendmail start
 
+#Apache2
 source /etc/apache2/envvars && exec /usr/sbin/apache2 -D FOREGROUND
